7e69db920fa69ce4bbc965b496b7c72e58514538
Leigh Simpson <code@simpleigh.com>
2013-03-27 22:53:30 +0000

    Added more byte stream tests.

--- tests/conversions/byte_stream.c
+++ tests/conversions/byte_stream.c
@@ -12,6 +12,39 @@ START_TEST(test_allocates_size)
 
 	fail_unless(result == CONVERSION_SUCCESS, "Failed to allocate");
 	fail_unless(stream.bytec == 5, "Stream length was %u, not 5", stream.bytec);
+
+	free_byte_stream(&stream);
+}
+END_TEST
+
+START_TEST(test_freeing_stream)
+{
+	struct byte_stream stream;
+
+	allocate_byte_stream(&stream, 5);
+	fail_unless(stream.bytec == 5, "Stream length was %u, not 5", stream.bytec);
+
+	free_byte_stream(&stream);
+	fail_unless(stream.bytec == 0, "Stream length not reset");
+	fail_unless(stream.bytev == NULL, "Stream pointer not NULL");
+}
+END_TEST
+
+START_TEST(test_zeroes_stream)
+{
+	struct byte_stream stream;
+	size_t i;
+
+	allocate_byte_stream(&stream, 5);
+	zero_byte_stream(&stream);
+
+	fail_unless(stream.bytec == 5, "Stream length was %u, not 5", stream.bytec);
+
+	for (i = 0; i < 5; i++) {
+		fail_unless(stream.bytev[i] == 0, "Byte %u not zeroed", i);
+	}
+
+	free_byte_stream(&stream);
 }
 END_TEST
 
@@ -21,6 +54,8 @@ conversions_byte_stream_suite(void)
 	Suite *s = suite_create("Conversions: byte stream manipulation");
 	TCase *tc_core = tcase_create("Core");
 	tcase_add_test(tc_core, test_allocates_size);
+	tcase_add_test(tc_core, test_freeing_stream);
+	tcase_add_test(tc_core, test_zeroes_stream);
 	suite_add_tcase(s, tc_core);
 	return s;
 }
